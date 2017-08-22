383ef4ef43cb5882b850133ff881dfe74c48673e
Kraken <cedric.cescutti@epitech.eu>
2017-08-22 21:41:34 +0200

    add api.ai

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 // Login   <cedric.cescutti@epitech.eu>
 // 
 // Started on  Sat Aug 12 11:27:42 2017 Kraken
-// Last update Tue Aug 22 21:39:04 2017 Kraken
+// Last update Tue Aug 22 21:41:26 2017 Kraken
 //
 
 var express = require('express');
@@ -109,6 +109,7 @@ function getName(event) {
 			console.log("error getting username");
 		} else {
 			var bodyObj = JSON.parse(body);
+			console.log(bodyObj);
 			firstName = bodyObj.first_name;
 			lastName = bodyObj.last_name;
 			sendTextMessage(sender_id, "Bonjour," + firstName + " " + lastName + ", je suis un bot créé par Jules et je vais vous trouver l\'ordinateur idéal.");
