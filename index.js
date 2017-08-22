c2be9efb61458c1e00ff0e2d3e78d0df3fb70ba2
Kraken <cedric.cescutti@epitech.eu>
2017-08-22 15:23:31 +0200

    add api.ai

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 // Login   <cedric.cescutti@epitech.eu>
 // 
 // Started on  Sat Aug 12 11:27:42 2017 Kraken
-// Last update Tue Aug 15 20:30:26 2017 Kraken
+// Last update Tue Aug 22 15:23:12 2017 Kraken
 //
 
 var express = require('express');
@@ -13,7 +13,7 @@ var bodyParser = require('body-parser');
 var request = require('request');
 
 var apiAi_token = "e6cf31a0e6a3440c81b4e7bda0d67442";
-var facebook_token = "my_voice_is_Pmy_password_verify_me";
+var facebook_token = 'my_voice_is_Pmy_password_verify_me';
 var heroku_token = "EAAbaupnhaZCwBAIgobay0MpBHY69qZAlmxd5JsFbCHQmX6gBHQKgX40287ov6stg6E0vLln7WsiZC3wlPHwZCYJgZCTtOcHCNaHLMQTQmhpOcpaMXQLSTZAZCOeUykWJaIqtfLIXtbt0phVoFel2T4Tj8i4lhdLliJQWWYHkNxEzwZDZD";
 
 var app = express();
@@ -51,6 +51,7 @@ app.post('/webhook/', (request, response) => {
 		event = request.body.entry[0].messaging_events[i];
 		sender = event.sender.id;
 		if (event.message && event.message.text) {
+			console.log("message reçu ->" + event.message.text);
 			sendToAi(event);
 		}
 	}
