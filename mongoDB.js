a16cbe00de5f689341fbe9754fb02bbf65cd6bdf
Cédric Cescutti <cedric.cescutti@epitech.eu>
2019-05-13 15:40:59 +0200

    Update mongoDB.js

--- mongoDB.js
+++ mongoDB.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var uri = "mongodb://kraken:<ns9r^fm@cluster0-shard-00-00-iw9xa.mongodb.net:27017,cluster0-shard-00-01-iw9xa.mongodb.net:27017,cluster0-shard-00-02-iw9xa.mongodb.net:27017/Pc_Db?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+var uri = "mongodb://kraken:@cluster0-shard-00-00-iw9xa.mongodb.net:27017,cluster0-shard-00-01-iw9xa.mongodb.net:27017,cluster0-shard-00-02-iw9xa.mongodb.net:27017/Pc_Db?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 MongoClient.connect(uri, function(err, db) {
 	// Paste the following examples here
 	db.collection('inventory').insertMany([
